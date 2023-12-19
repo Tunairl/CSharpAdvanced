cec4888c4b203efbbb393bacfd4451122c41bdc2
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-20 00:30:31 +0300

    Add files via upload

---
+++ AdvancedTopics/Delegates/Delegates/PhotoProccessor.cs
@@ -0,0 +1,17 @@
+﻿namespace Delegates
+{
+
+    public class PhotoProccessor
+    {
+
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+
+    }
+}
