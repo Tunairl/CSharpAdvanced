2e47015445dead9aefc1999e2fcf170ba0c4eba9
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-20 14:17:30 +0300

    Add files via upload

---
+++ AdvancedTopics/LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // args => expression
+            // => "goes to"
+
+            // () => ... (if we dont need an argument)
+            // x => ... (just one argument)
+            // (x, y, z) => ... (multiple args)
+
+            var books = new BookRepository().GetBooks();
+
+            var cheapBooks = books.FindAll(b => b.Price < 10); // there used to be an IsCheaperThan10Dollars method between FindAll parameter
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
