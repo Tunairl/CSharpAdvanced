888697b6024ff8d18d192d3c8510486cb4525942
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-21 01:20:08 +0300

    Add files via upload

---
+++ AdvancedTopics/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very very very long blog post.";
+            var shortenedPost = post.Shorten(8);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 2, 3, 10, 19 };
+            var max = numbers.Max();
+
+            Console.WriteLine(shortenedPost);
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ AdvancedTopics/ExtensionMethods/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace System // extensionmethods could be in the System namespace
+{ // MSDN says create Extension Methods ONLY when it's REALLY necessary
+    public static class StringExtensions
+    { 
+        // the extended methods should be PUBLIC STATIC
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords <= 0)
+            {
+                throw new ArgumentOutOfRangeException("numberOfWords should be greater than 0.");
+            }
+
+            var words = str.Split(' ');
+            if (words.Length <= numberOfWords) 
+            {
+                return str;
+            }
+
+            return string.Join(" ", words.Take(numberOfWords)) + "...";
+        }
+    }
+}
