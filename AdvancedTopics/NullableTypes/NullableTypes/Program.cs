0b688af8454115522c5d5ee4c37e234177a60d3d
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-21 16:46:03 +0300

    Add files via upload

---
+++ AdvancedTopics/NullableTypes/NullableTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = null;
+            DateTime? date2 = date ?? DateTime.Today;
+
+            DateTime date3 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
+            
+
+            Console.WriteLine(date2);
+        }
+    }
+}
