93aec6b583ad5f68de30179dc554ed0e870bed93
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-21 11:40:07 +0300

    Add files via upload

---
+++ AdvancedTopics/LINQ/Linq/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Linq
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            // LINQ Extension Methods
+            books.Where();
+            books.Single();
+            books.SingleOrDefault();
+
+            books.First();
+            books.FirstOrDefault();
+
+            books.Last();
+            books.LastOrDefault();
+
+            books.Min();
+            books.Max();
+            books.Count();
+            books.Sum();
+            books.Average(b => b.Price);
+
+            books.Skip(1).Take(3);
+           
+        }
+    }
+}
